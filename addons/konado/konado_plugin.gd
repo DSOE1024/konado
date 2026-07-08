8938627e59ee88f5773e0928e0b65ae1aa848772
DSOE1024 <computerlub@163.com>
2026-07-08 16:03:14 +0800

    chore: release konado v2.5.0
    update version number to 2.5.0 and codename to Diguoji, remove unused boot splash image config

--- addons/konado/konado_plugin.gd
+++ addons/konado/konado_plugin.gd
@@ -3,8 +3,8 @@ extends EditorPlugin
 class_name KonadoEditorPlugin
 # Konado框架入口文件，负责初始化插件和注册相关功能
 
-const VERSION: String = "2.4.0"
-const CODENAME: String = "Cannoli"
+const VERSION: String = "2.5.0"
+const CODENAME: String = "Diguoji"
 
 ## 自定义EditorImportPlugin脚本
 const KS_IMPORTER_SCRIPT := preload("uid://rp35gse7j4sv")
